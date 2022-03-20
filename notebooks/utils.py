93d45aabd5d0494918ddd052a8c6a32fe19fd01e
Robert Rustia <analyticalmr.r@gmail.com>
2022-03-20 02:19:09 -1000

    towards modeling annotations

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -114,3 +114,38 @@ def radar_compare(player1,player2,params, df):
     endnote = endnote
     )
     
+    
+# function to viz skill groups and players most associated with skill group
+
+def display_features(H,W,feature_names, X_matrix ,no_top_features, no_top_players):
+    import numpy as np
+    """ visualize skill get group and highest ranked players in group """
+    topics = {}
+    # iterate through topics in topic-term matrix, 'H' aka
+    # H is the hidden layer which is shape (F x C) feature times topic matrix
+    for topic_idx, topic in enumerate(H):
+        print("Topic %d:" % (topic_idx))
+        print(" ".join([ (feature_names[i] + " (" + str(topic[i].round(2)) + ")")
+          for i in topic.argsort()[:-no_top_features - 1:-1]]))
+        
+        # add features to topics dictionary for later assesment. 
+        
+        topics[topic_idx] = [ (feature_names[i] + " (" + str(topic[i].round(2)) + ")") 
+                             for i in topic.argsort()[:-(no_top_features+3) - 1:-1]]
+        
+        top_player_indicies = np.argsort( W[:,topic_idx] )[::-1][0:no_top_players]
+        for p_index in top_player_indicies:
+            
+            print(p_index," ",X_matrix.index[p_index])
+    
+    return topics 
+
+
+def add_skill_group(X,W):
+    import numpy as np
+    """ add skill group to player feature to use for classification"""
+    df_new = X.copy()
+    # Get the top predicted topic and add to df copy 
+    df_new['pred_topic_num']= [np.argsort(each)[::-1][0] for each in W]
+  
+    return df_new
